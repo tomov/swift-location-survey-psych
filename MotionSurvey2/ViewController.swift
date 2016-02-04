47a8011c3087de66be2c8f1cda8ed45799bd9cc5
Momchil Tomov <momchil@memsql.com>
2016-02-03 19:45:04 -0800

    simple location retrieval

--- MotionSurvey2/ViewController.swift
+++ MotionSurvey2/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    private var locMgr = CLLocationManager()
+    private var geo = CLGeocoder()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locMgr.delegate = self
+        locMgr.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locMgr.requestWhenInUseAuthorization() // for foreground
+       // locMgr.requestAlwaysAuthorization() // for background
+        
+        /*if (CLLocationManager.locationServicesEnabled()) {
+            locMgr.startMonitoringSignificantLocationChanges();
+        } else {
+            print("Location services not enabled, please enable this in your Settings");
+        }
+        */
+    }
+    
+    @IBAction func getFixPressed(sender: AnyObject) {
+        locMgr.requestLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: CLLocationManagerDelegate
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+    }
 }
 
