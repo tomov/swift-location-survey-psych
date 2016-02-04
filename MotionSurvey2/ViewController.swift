7306118f684e7d37ede2bc14a4f5c538a799cf47
Momchil Tomov <momchil@memsql.com>
2016-02-03 21:00:40 -0800

    startUpdateLocation but in foreground only

--- MotionSurvey2/ViewController.swift
+++ MotionSurvey2/ViewController.swift
@@ -14,25 +14,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     private var locMgr = CLLocationManager()
     private var geo = CLGeocoder()
 
+    @IBOutlet weak var locationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // setup locMgr
+        //
         locMgr.delegate = self
         locMgr.desiredAccuracy = kCLLocationAccuracyBest
         
+        // request permission to use locations
+        //
         locMgr.requestWhenInUseAuthorization() // for foreground
-       // locMgr.requestAlwaysAuthorization() // for background
+        locMgr.requestAlwaysAuthorization() // for background
         
-        /*if (CLLocationManager.locationServicesEnabled()) {
+        //locMgr.allowDeferredLocationUpdatesUntilTraveled(20.0, timeout: 5)  // meters, seconds (?)
+        
+        // foreground only -- receive updates every 20 meters
+        //
+        locMgr.distanceFilter = 20 // meters
+        locMgr.startUpdatingLocation()
+        
+       /* if (CLLocationManager.locationServicesEnabled()) {
             locMgr.startMonitoringSignificantLocationChanges();
         } else {
             print("Location services not enabled, please enable this in your Settings");
-        }
-        */
+        }*/
     }
     
     @IBAction func getFixPressed(sender: AnyObject) {
-        locMgr.requestLocation()
+        locMgr.requestLocation() // dosn't work if startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,12 +53,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
 
     // MARK: CLLocationManagerDelegate
+    
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // list of latest locations
+        //
         print(locations)
+        locationLabel.text = "\(locations[0])"
     }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
         print(error)
     }
+    
+    func locationManager(manager: CLLocationManager, didFinishDeferredUpdatesWithError error: NSError?) {
+        print(error)
+    }
+    
+    //
+    // TODO make it ask again if location services was disabled
 }
 
